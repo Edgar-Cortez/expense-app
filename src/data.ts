dbd165e7a320d8378e526821834f6d33109c8c13
EdgarCortez1 <ea.cortezz@gmail.com>
2023-07-12 11:55:59 -0500

    created a data object

---
+++ src/data.ts
@@ -0,0 +1,28 @@
+export const data: Data = {
+    report: []
+}
+
+interface Data {
+    report: {
+        id: string;
+        source: string;
+        amount: number;
+        created_at: Date;
+        updated_at: Date;
+        type: ReportType;
+    }[]
+}
+
+enum ReportType {
+    EXPENSE = "expense",
+    INCOME = "income"
+}
+
+data.report.push({
+    id: "uuid",
+    source: "Salary",
+    amount: 7500,
+    created_at: new Date(),
+    updated_at: new Date(),
+    type: ReportType.INCOME
+})
