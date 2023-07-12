3a00aada17639ef00b2d1a4a0d6e9d99710a9eec
EdgarCortez1 <ea.cortezz@gmail.com>
2023-07-12 12:28:54 -0500

    completed the gets

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,29 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { data, ReportType } from 'src/data';
 
 @Controller('report/:type')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getAllReports() {
-    return [];
+  getAllReports(@Param('type') type: string) {
+    console.log(type);
+    const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE;
+    return data.report.filter((report) => report.type === reportType);
   }
 
-  @Get (':id')
-  getIncomeReportById() {
-    return {};
+  @Get(':id')
+  getIncomeReportById(@Param('type') type: string, @Param('id') id: string) {
+    console.log({ type, id });
+    const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE;
+    return data.report
+      .filter((report) => report.type === reportType)
+      .find(report => report.id === id);
   }
 
   @Post()
-  createReport(){
+  createReport() {
     return 'Created';
   }
 
