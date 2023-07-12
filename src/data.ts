ac91757edb64269b1f0d566e5c3d7ac41c6b3e3e
EdgarCortez1 <ea.cortezz@gmail.com>
2023-07-12 12:27:23 -0500

    exported ReportType

--- src/data.ts
+++ src/data.ts
@@ -1,6 +1,44 @@
-export const data: Data = {
-    report: []
+export enum ReportType {
+    EXPENSE = "expense",
+    INCOME = "income"
 }
+
+export const data: Data = {
+    report: [
+        {
+            id: "uuid1",
+            source: "Salary",
+            amount: 7500,
+            created_at: new Date(),
+            updated_at: new Date(),
+            type: ReportType.INCOME
+        },
+        {
+            id: "uuid2",
+            source: "Irrigation Course",
+            amount: 10000,
+            created_at: new Date(),
+            updated_at: new Date(),
+            type: ReportType.INCOME
+        },
+        {
+            id: "uuid3",
+            source: "Groceries",
+            amount: 400,
+            created_at: new Date(),
+            updated_at: new Date(),
+            type: ReportType.EXPENSE
+        },
+        {
+            id: "uuid4",
+            source: "Credit Card Payment",
+            amount: 100,
+            created_at: new Date(),
+            updated_at: new Date(),
+            type: ReportType.EXPENSE
+        }
+    ],
+};
 
 interface Data {
     report: {
@@ -13,16 +51,4 @@ interface Data {
     }[]
 }
 
-enum ReportType {
-    EXPENSE = "expense",
-    INCOME = "income"
-}
 
-data.report.push({
-    id: "uuid",
-    source: "Salary",
-    amount: 7500,
-    created_at: new Date(),
-    updated_at: new Date(),
-    type: ReportType.INCOME
-})
