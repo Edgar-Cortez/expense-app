1fc000a6dac174a2aca1c5a114de4ae53155623d
Edgar Cortez <eac0001@dcccd.edu>
2023-07-20 16:49:58 -0500

    updated the id's for api to be able to find them

--- src/data.ts
+++ src/data.ts
@@ -1,54 +1,52 @@
 export enum ReportType {
-    EXPENSE = "expense",
-    INCOME = "income"
+  EXPENSE = 'expense',
+  INCOME = 'income',
 }
 
 export const data: Data = {
-    report: [
-        {
-            id: "uuid1",
-            source: "Salary",
-            amount: 7500,
-            created_at: new Date(),
-            updated_at: new Date(),
-            type: ReportType.INCOME
-        },
-        {
-            id: "uuid2",
-            source: "Irrigation Course",
-            amount: 10000,
-            created_at: new Date(),
-            updated_at: new Date(),
-            type: ReportType.INCOME
-        },
-        {
-            id: "uuid3",
-            source: "Groceries",
-            amount: 400,
-            created_at: new Date(),
-            updated_at: new Date(),
-            type: ReportType.EXPENSE
-        },
-        {
-            id: "uuid4",
-            source: "Credit Card Payment",
-            amount: 100,
-            created_at: new Date(),
-            updated_at: new Date(),
-            type: ReportType.EXPENSE
-        }
-    ],
+  report: [
+    {
+      id: 'ea887870-9835-4335-8dd9-758faba3481f',
+      source: 'Salary',
+      amount: 7500,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+    {
+      id: 'bebe5d11-b7d7-426f-b7ae-b3e9811df2a4',
+      source: 'Irrigation Course',
+      amount: 10000,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+    {
+      id: 'ad9356eb-60aa-449e-8003-b1704a7ab213',
+      source: 'Groceries',
+      amount: 400,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.EXPENSE,
+    },
+    {
+      id: '3839f39c-e04e-4be3-8c7e-9131197183c5',
+      source: 'Credit Card Payment',
+      amount: 100,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.EXPENSE,
+    },
+  ],
 };
 
 interface Data {
-    report: {
-        id: string;
-        source: string;
-        amount: number;
-        created_at: Date;
-        updated_at: Date;
-        type: ReportType;
-    }[]
+  report: {
+    id: string;
+    source: string;
+    amount: number;
+    created_at: Date;
+    updated_at: Date;
+    type: ReportType;
+  }[];
 }
-
-
