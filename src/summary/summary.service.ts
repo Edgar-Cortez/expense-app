7d6ad76b2b408c3e3b78d9a52f10b8166d387d42
Edgar Cortez <eac0001@dcccd.edu>
2023-07-20 16:20:38 -0500

    Setting everything up to calculate reports

--- src/summary/summary.service.ts
+++ src/summary/summary.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { ReportService } from 'src/report/report.service';
 
 @Injectable()
-export class SummaryService {}
+export class SummaryService {
+  constructor(private readonly reportService: ReportService) {}
+
+  calculateSummary() {}
+}
